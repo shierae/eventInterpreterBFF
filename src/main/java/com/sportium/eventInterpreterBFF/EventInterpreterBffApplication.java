82327e5160017e4366d47d50bd4592aa1f93c1dd
shierae <shieray@hotmail.com>
2024-04-01 11:41:38 +0200

    first commit - basic spring boot structure

---
+++ src/main/java/com/sportium/eventInterpreterBFF/EventInterpreterBffApplication.java
@@ -0,0 +1,13 @@
+package com.sportium.eventInterpreterBFF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventInterpreterBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventInterpreterBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportium/eventInterpreterBFF/EventInterpreterBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportium.eventInterpreterBFF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventInterpreterBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
